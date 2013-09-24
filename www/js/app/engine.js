10d7dfeb3559e0b8d081989551b55694ffb48287
Michael <michael@doublespeakgames.com>
2013-09-24 19:34:40 -0400

    Massively improved touch controls. Still need to optimize animations...

--- www/js/app/engine.js
+++ www/js/app/engine.js
@@ -2,16 +2,16 @@ define(['jquery', 'app/graphics', 'app/gameboard', 'app/events', 'app/gamestate'
 		function($, Graphics, GameBoard, Events, GameState, World) {
 
 	return {
+		DRAG_THRESHOLD: 30, // in pixels
 		activeTile: null,
-		mouse: {x: 0, y: 0},
+		dragging: false,
+		dragStart: {x: 0, y: 0},
 		
 		options: {},
 		init: function(opts) {
 			$.extend(this.options, opts);
 			
-			// Bind mouse events
-			$('document').off().mousemove(this.mouseMove);
-			$('document').off().mouseup(this.mouseUp);
+
 			
 //			$('#test').click(function() { require(['app/gameboard'], function(G) {
 //				console.log("Moves available? " + G.areMovesAvailable());
@@ -36,16 +36,38 @@ define(['jquery', 'app/graphics', 'app/gameboard', 'app/events', 'app/gamestate'
 			World.launchDude();
 			
 			var _engine = this;
-			GameBoard.el().on("mousedown", ".tile", function() {
-				var tile = $.data(this, "tile");
-				_engine.startDrag(tile); 
+			GameBoard.el().on("mousedown touchstart", ".tile", function(e) {
+				if(!_engine.dragging) {
+					// Handle wacky touch event objects
+					if(e.originalEvent.touches) {
+						e = e.originalEvent.touches[0];
+					}
+					_engine.dragStart.x = e.clientX;
+					_engine.dragStart.y = e.clientY;
+					_engine.dragging = true;
+					var tile = $.data(this, "tile");
+					_engine.startDrag(tile);
+				}
 				return false;
 			});
-			GameBoard.el().on("mouseup", ".tile", function() {
-				var tile = $.data(this, "tile");
-				_engine.endDrag(tile); 
+			GameBoard.el().on("mouseup touchend", function(e) {
+				if(_engine.dragging) {
+					_engine.dragging = false;
+					// Handle wacky touch event objects
+					if(e.originalEvent.touches) {
+						e = e.originalEvent.touches[0];
+					}
+					_engine.endDrag({ 
+						x: e.clientX - _engine.dragStart.x,
+						y: e.clientY - _engine.dragStart.y
+					});
+				}
 				return false;
 			});
+		},
+		
+		_debug: function(text) {
+			$('#debug').text(text);
 		},
 		
 		startDrag: function(tile) {
@@ -66,11 +88,13 @@ define(['jquery', 'app/graphics', 'app/gameboard', 'app/events', 'app/gamestate'
 			}
 		},
 		
-		endDrag: function(tile) {
+		endDrag: function(delta) {
 			if(this.activeTile != null) {
-				var dx = tile.options.column - this.activeTile.options.column;
+				var dx = delta.x / this.DRAG_THRESHOLD;
+				dx = dx < 0 ? Math.ceil(dx) : Math.floor(dx);
 				dx = dx / Math.abs(dx) || 0;
-				var dy = tile.options.row - this.activeTile.options.row;
+				var dy = delta.y / this.DRAG_THRESHOLD;
+				dy = dy < 0 ? Math.ceil(dy) : Math.floor(dy);
 				dy = dy / Math.abs(dy) || 0;
 				if(Math.abs(dx) + Math.abs(dy) == 1) {
 					var active = this.activeTile;
