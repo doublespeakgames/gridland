22a563fef2899a34e430e848de869d84002eb254
Michael <michael@doublespeakgames.com>
2013-11-02 10:22:54 -0400

    Fixed a bug with swords

--- www/js/app/action/attack.js
+++ www/js/app/action/attack.js
@@ -23,7 +23,7 @@ define(['app/action/action'], function(Action) {
 	
 	Attack.prototype.doFrameAction = function(frame) {
 		if(frame == 1) {
-			this.target.takeDamage(this._entity.getDamage(this.target.hp));
+			this.target.takeDamage(this._entity.getDamage(this.target.hp()));
 		} else if(frame == 3) {
 			this._entity.action = null;
 		}
