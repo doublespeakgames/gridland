d931854c266b8611dcc99a428a21fbcd00197829
Michael! <michael@doublespeak.(none)>
2014-05-22 00:06:05 -0400

    Starting on donations

--- www/js/app/graphics/donate.js
+++ www/js/app/graphics/donate.js
@@ -1,7 +1,14 @@
 define(function() {
 	
 	var providers = [
-
+		'<form method="post" action="https://www.paypal.com/cgi-bin/webscr">' +
+			'<input type="hidden" value="_donations" name="cmd">' +
+			'<input type="hidden" value="continuities@gmail.com" name="business">' +
+			'<input type="hidden" value="Donation to doublespeak games" name="item_name">' +
+			'<input type="hidden" value="0" name="rm">' +
+			'<input type="hidden" value="CAD" name="currency_code">' +
+	        '<input type="image" alt="PayPal" name="submit" class="paypal">' +
+		'</form>'
 	];
 	
 	var _el = null;
@@ -11,7 +18,7 @@ define(function() {
 			G.addStyleRule('.donate:before', 'content:"- ' + G.getText('DONATE') + ' -";');
 			_el = G.make('donate submenu', 'ul');
 			providers.forEach(function(link) {
-				// TODO
+				
 			});
 		}
 		
