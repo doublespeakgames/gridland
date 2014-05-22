ab464b77051ac66b052edd85b58d9ee2abda6abe
Michael <michael@doublespeakgames.com>
2014-05-22 01:22:03 -0400

    Added donate buttons!

--- www/js/app/graphics/donate.js
+++ www/js/app/graphics/donate.js
@@ -1,14 +1,30 @@
 define(function() {
 	
 	var providers = [
-		'<form method="post" action="https://www.paypal.com/cgi-bin/webscr">' +
+		'<form method="post" target="_blank" action="https://www.paypal.com/cgi-bin/webscr">' +
 			'<input type="hidden" value="_donations" name="cmd">' +
 			'<input type="hidden" value="continuities@gmail.com" name="business">' +
 			'<input type="hidden" value="Donation to doublespeak games" name="item_name">' +
 			'<input type="hidden" value="0" name="rm">' +
 			'<input type="hidden" value="CAD" name="currency_code">' +
-	        '<input type="image" alt="PayPal" name="submit" class="paypal">' +
-		'</form>'
+	        '<input type="image" alt="" title="PayPal" name="submit" class="paypal nightSprite">' +
+	        '<input type="image" alt="" class="paypal"></input>' + 
+		'</form>',
+		'<a class="flattr nightSprite" title="Flattr" target="_blank" href="https://flattr.com/thing/1570114/doublespeak-games"></a>' + 
+		'<input type="image" alt="" class="flattr"></input>',
+		'<div class="bitcoin">' +
+			'<a href="bitcoin:151Ch7PwzMtiVEHMYth5F9REmySvxKJBDN" ' +
+				'data-info="none" data-address="151Ch7PwzMtiVEHMYth5F9REmySvxKJBDN" ' +
+				'class="bitcoin-button nightSprite" target="_blank"></a>' +
+			'<input type="image" alt="" class="bitcoin-button"></input>' + 
+			'<div class="bitcoin-bubble">' +
+				'<img width="200" height="200" alt="QR code" ' +
+					'src="http://chart.googleapis.com/chart?chs=200x200&amp;cht=qr&amp;chld=H|0&amp;chl=bitcoin%3A151Ch7PwzMtiVEHMYth5F9REmySvxKJBDN">' +
+				'151Ch7PwzMtiVEHMYth5F9REmySvxKJBDN' + 
+			'</div>' +
+		'</div>',
+		'<a class="gittip nightSprite" title="Gittip" target="_blank" href="https://www.gittip.com/Continuities/"></a>' +
+		'<input type="image" alt="" class="gittip"></input>'
 	];
 	
 	var _el = null;
@@ -18,7 +34,7 @@ define(function() {
 			G.addStyleRule('.donate:before', 'content:"- ' + G.getText('DONATE') + ' -";');
 			_el = G.make('donate submenu', 'ul');
 			providers.forEach(function(link) {
-				
+				G.make('litBorder', 'li').html(link).appendTo(_el);
 			});
 		}
 		
