eb84cca15a409b9c0570b37352c31f13029f45cc
Michael <michael@doublespeakgames.com>
2014-06-04 18:22:27 -0400

    Added day number to tracking to make balancing easier.

--- www/js/app/analytics.js
+++ www/js/app/analytics.js
@@ -33,12 +33,15 @@ define(["google-analytics", "app/eventmanager"], function(ga, E) {
 			}
 			
 			if(initialized) {
-				E.bind('newGame', function() { trackEvent('game', 'newgame'); });
-				E.bind('dayBreak', function(num) { trackEvent('game', 'daybreak', num); });
-				E.bind('dudeDeath', function(monster) { trackEvent('game', 'death', monster); });
-				E.bind('buildingComplete', function(building) { trackEvent('game', 'build', building.options.type.className); });
-				E.bind('gameOver', function() { trackEvent('game', 'complete'); });
-				E.bind('levelUp', function(level) { trackEvent('game', 'levelup', level); });
+				
+				var S = require('app/gamestate');
+				
+				E.bind('newGame', function() { trackEvent('game', 'new'); });
+				E.bind('dayBreak', function(num) { trackEvent('daybreak', null, num); });
+				E.bind('dudeDeath', function(monster) { trackEvent('death', null, monster); });
+				E.bind('buildingComplete', function(building) { trackEvent('build', building.options.type.className, S.dayNumber); });
+				E.bind('gameOver', function() { trackEvent('game', 'complete', S.dayNumber); });
+				E.bind('levelUp', function(level) { trackEvent('levelup', level, S.dayNumber); });
 				E.bind('click', function(thing) { trackEvent('click', thing); });
 				E.bind('prestige', function() { trackEvent('game', 'prestige'); });
 			}
