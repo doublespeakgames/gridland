d62eda5b334b7b59fc865effd68ac2d6cb4566df
Michael Townsend <michael@doublespeakgames.com>
2014-09-06 14:34:58 -0400

    missed a file

---
+++ www/js/app/graphics/difficulty.js
@@ -0,0 +1,34 @@
+define(['app/eventmanager', 'app/gameoptions'], function(E, O) {
+
+	var G = null, _el = null;
+
+	function el() {
+		if(_el == null) {
+			G.addStyleRule('.difficulty:before', 'content:"- ' + G.getText('DIFFICULTY') + ' -";');
+			_el = G.make('difficulty submenu').append(G.make('difficultySwitch litBorder'));
+			G.addToMenu(_el);
+		}
+		return _el;
+	}
+
+	function toggleSwitch() {
+		O.set('casualMode', !O.get('casualMode', false));
+		syncSwitch();
+	}
+
+	function syncSwitch() {
+		el().toggleClass('casual', O.get('casualMode', false));
+	}
+
+	return {
+		init: function() {
+			G = require('app/graphics/graphics');
+			if(_el) {
+				_el.remove();
+			}
+			_el = null;
+			el().off().on('click touchstart', toggleSwitch)
+			syncSwitch();
+		}
+	}
+});
