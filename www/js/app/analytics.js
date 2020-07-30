be3f2149b13a4ef40c874925008ee3b7a27c9836
Michael Townsend <michael@doublespeakgames.com>
2020-07-30 16:58:32 -0400

    work without GA

--- www/js/app/analytics.js
+++ www/js/app/analytics.js
@@ -1,5 +1,5 @@
-define(["google-analytics", "app/eventmanager"], function(ga, E) {
-	
+define(["app/eventmanager"], function(E) {
+
 	function trackPageView(uri) {
 		if(initialized) {
 			ga('send', 'event', 'pageView', uri);
@@ -22,30 +22,32 @@ define(["google-analytics", "app/eventmanager"], function(ga, E) {
 	var initialized = false;
 	return {
 		init: function() {
-			if(!initialized) {
-				try {
-					ga('create', 'UA-41314886-2', 'doublespeakgames.com');
-					ga('send', 'pageview');
-					initialized = true;
-				} catch(e) {
-					console.log("Failed to initialize Google Analytics: " + e.message);
-				}
-			}
-			
-			if(initialized) {
-				
-				var S = require('app/gamestate');
-				
-				E.bind('newGame', function() { trackEvent('game', 'new'); });
-				E.bind('dayBreak', function(num) { trackEvent('daybreak', num, num); });
-				E.bind('dudeDeath', function(monster) { trackEvent('death', monster, S.dayNumber); });
-				E.bind('buildingComplete', function(building) { trackEvent('build', building.options.type.className, S.dayNumber); });
-				E.bind('gameOver', function() { trackEvent('game', 'complete', S.dayNumber); });
-				E.bind('levelUp', function(level) { trackEvent('levelup', level, S.dayNumber); });
-				E.bind('click', function(thing) { trackEvent('click', thing); });
-				E.bind('prestige', function() { trackEvent('game', 'prestige'); });
-				E.bind('keySequenceComplete', function() { trackEvent('game', 'codeEntered'); });
-			}
-		}
+      require(['google-analytics'], function(ga) {
+        if(!initialized) {
+          try {
+            ga('create', 'UA-41314886-2', 'doublespeakgames.com');
+            ga('send', 'pageview');
+            initialized = true;
+          } catch(e) {
+            console.log("Failed to initialize Google Analytics: " + e.message);
+          }
+        }
+        
+        if(initialized) {
+          
+          var S = require('app/gamestate');
+          
+          E.bind('newGame', function() { trackEvent('game', 'new'); });
+          E.bind('dayBreak', function(num) { trackEvent('daybreak', num, num); });
+          E.bind('dudeDeath', function(monster) { trackEvent('death', monster, S.dayNumber); });
+          E.bind('buildingComplete', function(building) { trackEvent('build', building.options.type.className, S.dayNumber); });
+          E.bind('gameOver', function() { trackEvent('game', 'complete', S.dayNumber); });
+          E.bind('levelUp', function(level) { trackEvent('levelup', level, S.dayNumber); });
+          E.bind('click', function(thing) { trackEvent('click', thing); });
+          E.bind('prestige', function() { trackEvent('game', 'prestige'); });
+          E.bind('keySequenceComplete', function() { trackEvent('game', 'codeEntered'); });
+        }
+      });
+    }
 	};
 });
